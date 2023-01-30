07de545f42854af708d9e0dc3eb1e0c9bdef67ad
Jayanthbabu456 <babujayanth456@gmail.com>
2023-01-30 19:31:54 +0530

    code

---
+++ JavaScript_Assignments/01 QUESTION/variable.js
@@ -0,0 +1,17 @@
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+//string
+let name = "Jayanth";
+console.log("name:", name, "type:", typeof name);
+
+//boolean
+let isValid = true;
+console.log("isValid:", isValid, "type:", typeof isValid);
+
+//undefined
+let age;
+console.log("age:", age, "type:", typeof age);
+
+//null
+let address = null;
+console.log("address:", address, "type:", typeof address);
