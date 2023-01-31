c80c346cd79c0c7f649232316bb03fadbc5f1fcc
Jayanthbabu456 <babujayanth456@gmail.com>
2023-01-31 21:03:32 +0530

    code

---
+++ JavaScript_Assignments/02 QUESTION/interpolation.js
@@ -0,0 +1,10 @@
+//Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName = "Jayanth";
+let lastName = "Babu";
+let maritalStatus = "unmarried";
+let country = "India";
+let age = 20;
+console.log(
+  `My name is ${firstName} ${lastName}. I am a ${maritalStatus} from ${country} and I am ${age} years old.`
+);
