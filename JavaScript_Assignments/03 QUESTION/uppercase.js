d3865083721440757103750484158e831acc22d6
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-01 20:08:21 +0530

    code

---
+++ JavaScript_Assignments/03 QUESTION/uppercase.js
@@ -0,0 +1,5 @@
+//Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let name = "jayanth";
+let uppercase = name.toUpperCase();
+console.log(uppercase);
