638495c9ff3e3e3ad5d1d2e0210e1d68d2592927
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-02 19:48:48 +0530

    code

---
+++ JavaScript_Assignments/04 QUESTION/include.js
@@ -0,0 +1,5 @@
+//Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+let stringName = "Hello,I am script";
+let finds = stringName.includes("script");
+console.log(finds);
