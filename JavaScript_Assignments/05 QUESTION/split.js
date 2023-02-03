a92109a0db3033d7c60f5d024a9f00122324b444
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-03 19:21:11 +0530

    code

---
+++ JavaScript_Assignments/05 QUESTION/split.js
@@ -0,0 +1,5 @@
+//Declare a varibale and assign string value to it and then split it into an array using split() method
+
+let str = "FULLSTACK JAVASCRIPT BOOTCAMP2.0";
+let arr = str.split(" ");
+console.log(arr);
