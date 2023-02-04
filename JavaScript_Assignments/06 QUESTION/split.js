ae4ab5d7256303531420d6a3d59dbd778d77810f
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-04 20:28:17 +0530

    code

---
+++ JavaScript_Assignments/06 QUESTION/split.js
@@ -0,0 +1,5 @@
+//'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let companiesArray = companies.split(", ");
+console.log(companiesArray);
