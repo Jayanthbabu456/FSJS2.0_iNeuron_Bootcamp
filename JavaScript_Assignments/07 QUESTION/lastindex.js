be81bb27a43fde56efea306cb5f1a7c1f1f2a3ee
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-05 20:58:42 +0530

    code

---
+++ JavaScript_Assignments/07 QUESTION/lastindex.js
@@ -0,0 +1,7 @@
+//Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+var array = ["pw skills", "pw skills", "Learning", "Learning", "pw skills"];
+var firstIndex = array.indexOf("pw skills");
+var lastIndex = array.lastIndexOf("pw skills");
+console.log("firstIndex of pwskills", firstIndex);
+console.log("lastIndex of pwskills", lasttIndex);
