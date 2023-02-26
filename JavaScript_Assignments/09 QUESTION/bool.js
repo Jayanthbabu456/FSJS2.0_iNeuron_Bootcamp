c052fd3dd9a93a884f270ce54070af8d493e9b76
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-26 21:32:22 +0530

    9th question

---
+++ JavaScript_Assignments/09 QUESTION/bool.js
@@ -0,0 +1,15 @@
+// Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+// Truthy values:
+
+let x = 5; //Assigning a numerical value to a variable provides a truthy value.
+const str = "Hello, world!"; //Assigning a string value to a constant variable provides a truthy value.
+let arr = [1, 2, 3]; //Assigning an array of values to a variable provides a truthy value.
+
+//Falsy values:
+
+let y = 0; //Assigning a numerical value of zero to a variable provides a falsy value.
+const bool = false; //Assigning the boolean value false to a constant variable provides a falsy value.
+let emptyStr = ""; //Assigning an empty string to a variable provides a falsy value.
