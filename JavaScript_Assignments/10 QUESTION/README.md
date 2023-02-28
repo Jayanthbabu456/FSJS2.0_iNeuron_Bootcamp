415171ef6555e01b4f0dfd09da6682ae1c2baad3
Jayanthbabu456 <babujayanth456@gmail.com>
2023-02-28 11:02:48 +0530

    solution

--- JavaScript_Assignments/10 QUESTION/README.md
+++ JavaScript_Assignments/10 QUESTION/README.md
@@ -1,1 +1,29 @@
 # 10 QUESTION
+
+# OUTPUT
+
+## 4 > 3; -true
+
+## 4 >= 3; -true
+
+## 4 < 3; -false
+
+## 4 <= 3; -false
+
+## 4 == 4; -true
+
+## 4 === 4; -true
+
+## 4 != 4; -false
+
+## 4 !== 4; -false
+
+## 4 != "4"; -false
+
+## 4 == "4"; -true
+
+## 4 === "4"; -false
+
+## length of python and jargon and make a falsy comparison statement.
+
+## false
---
+++ JavaScript_Assignments/10 QUESTION/compare.js
@@ -0,0 +1,44 @@
+// Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3
+//     - 4 >= 3
+//     - 4 < 3
+//     - 4 <= 3
+//     - 4 == 4
+//     - 4 === 4
+//     - 4 != 4
+//     - 4 !== 4
+//     - 4 != '4'
+//     - 4 == '4'
+//     - 4 === '4'
+
+// EXPECTED SOLUTIONS:-
+4 > 3; //true
+4 >= 3; //true
+4 < 3; //false
+4 <= 3; //false
+4 == 4; //true
+4 === 4; //true
+4 != 4; //false
+4 !== 4; //false
+4 != "4"; //false
+4 == "4"; //true
+4 === "4"; //false
+
+//OUTPUT:
+console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 !== 4);
+console.log(4 != "4");
+console.log(4 == "4");
+console.log(4 === "4");
+
+//     - Find the length of python and jargon and make a falsy comparison statement.
+
+s1 = "python";
+s2 = "jargon";
+console.log(s1.length < s2.length);
