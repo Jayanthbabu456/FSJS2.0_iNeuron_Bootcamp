fad9fc84ba9f519fb7b7c815ff361d1e7263f3f7
Jayanthbabu456 <babujayanth456@gmail.com>
2023-03-22 19:37:38 +0530

    25 Solution

---
+++ JavaScript_Assignments/25 QUESTION/README.md
@@ -0,0 +1,5 @@
+# 25 QUESTION
+
+## OUTPUT
+
+## BMI: 23.53. You are Normal weight.
---
+++ JavaScript_Assignments/25 QUESTION/items.js
@@ -0,0 +1,24 @@
+// Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+//- The same groups apply to both men and women.
+//- Underweight: BMI is less than 18.5
+//- Normal weight: BMI is 18.5 to 24.9
+//- Overweight: BMI is 25 to 29.9
+//- Obese: BMI is 30 or more
+
+function calculateBMI(weight, height) {
+  const bmi = weight / (height * height);
+  let message = "";
+  if (bmi < 18.5) {
+    message = "Underweight";
+  } else if (bmi < 25) {
+    message = "Normal weight";
+  } else if (bmi < 30) {
+    message = "Overweight";
+  } else {
+    message = "Obese";
+  }
+  return `BMI: ${bmi.toFixed(2)}. You are ${message}.`;
+}
+
+// Example usage
+console.log(calculateBMI(68, 1.7)); // BMI: 23.53. You are Normal weight.
